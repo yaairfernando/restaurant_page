f24da4479f64622c5a0a63972125ca2500706377
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-04-13 16:29:23 -0500

    Rendered tabs

--- src/homepage.js
+++ src/homepage.js
@@ -10,6 +10,7 @@ const homepage = (() => {
     container.appendChild(review());
     h1.textContent = "LA COZZA INFURIATA"
     container.insertBefore(h1,document.querySelector('.review-container'));
+    container.appendChild(tabs())
   }
 
   const review = () => {
@@ -28,6 +29,28 @@ const homepage = (() => {
     p2.textContent = "When New Yorkers hear the word brisket these days, they tend to think of brick pits, stacks of cordwood, neon Shiner Beers signs, and Willie and Waylon singing in the background."
     p3.textContent = "Those associations are relatively recent ones locally, dating to the renaissance of the smoked-meat sciences in Texas and the national fame of pit masters like Aaron Franklin in Austin.Before brisket was synonymous with barbecue, its meaning in the five boroughs was the one that you now encounter mainly around the Jewish holidays: a flat slab of beef breast cooked in a closed, humid environment for hours, at which point it not only falls apart unprovoked but also makes its own gravy."
     return reviewContainer
+  }
+
+  const tabs = () => {
+    let tabsContianer = document.createElement('div');
+    tabsContianer.classList.add('tabs-container');
+    let tabOne = document.createElement('div');
+    let tabTwo = document.createElement('div');
+    tabOne.classList.add('tab-one')
+    tabTwo.classList.add('tab-two')
+    tabOne.id = 'tab-one'
+    tabTwo.id = 'tab-two'
+    let linkOne = document.createElement('a');
+    let linkTwo = document.createElement('a');
+    linkOne.href = '#'
+    linkTwo.href = '#'
+    linkOne.textContent = 'Menu'
+    linkTwo.textContent = 'Contact Us'
+    tabOne.appendChild(linkOne)
+    tabTwo.appendChild(linkTwo)
+    tabsContianer.appendChild(tabTwo)
+    tabsContianer.insertBefore(tabOne, tabTwo);
+    return tabsContianer
   }
   return { render }
 })();
