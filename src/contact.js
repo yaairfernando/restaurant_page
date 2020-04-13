36490cf465f8e94ada0eb40398af3be1c517cd09
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-04-13 17:40:26 -0500

    Finished contact page and refactor to use string literal

---
+++ src/contact.js
@@ -0,0 +1,34 @@
+import '../styles/styles.css'
+
+const contact = (() => {
+  const contactPage = () => {
+    if (!document.querySelector('.contact-container')) {
+      document.querySelector('#tab-two').classList.add('active');
+      document.querySelector('#tab-one').classList.remove('active');
+      let contactContainer = document.createElement('div');
+      contactContainer.classList.add('contact-container');
+      contactContainer.innerHTML = info();
+      document.querySelector('#content').appendChild(contactContainer);
+    }
+  }
+
+  const info = () => {
+    let info =  `
+      <div class="info">
+        <p>cozzainfuriata@gmail.com</p>
+        <p>842 256 2568</p>
+        <p>8036 Mechanicsville Turnpike, Mechanicsville, VA 23111</p>
+      </div>
+      <div class="social">
+        <div class="social-item"></div>
+        <div class="social-item"></div>
+      </div>
+    `
+    return info
+  }
+
+
+  return { contactPage }
+})()
+
+export default contact;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import homepage from './homepage';
+import contact from './contact';
 
 homepage.render();
+
+document.querySelector('#tab-one').addEventListener('click', () => {
+  console.log("click");
+})
+
+document.querySelector('#tab-two').addEventListener('click', () => {
+  contact.contactPage();
+})
